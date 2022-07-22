77019176c935e07c9ba465f8793092d8129e36b0
franponce <franponce98@gmail.com>
2022-07-22 15:49:41 -0300

    Se instala axios + dotenv, se configura dotenv en el app.js

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # APIMercadoPago
+
+JS con Express 
+
+Axios para las requests 
+Dotenv para proteger nuestra api key
--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const dotenv = require("dotenv")
 
 var indexRouter = require('./routes/index');
 
 var app = express();
+
+dotenv.config();
 
 app.use(logger('dev'));
 app.use(express.json());
